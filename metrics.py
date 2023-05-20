0120c8a2d77a4c55d88716d8bd5e4500c126ea4a
Elin Angelov <opensuser@gmail.com>
2023-05-20 19:52:46 +0300

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,7 +1,12 @@
 import urequests
 
  # https://docs.influxdata.com/influxdb/cloud/api/
-def rq(config, data):
+def influxdb(config, data):
     print("send metrics")
-    r = urequests.post(config["url"], headers={"Authorization": " Token" + config["authorization"]})
+    qs = "org=" + config["org"]
+    qs += "&bucket=" + config["bucket"]
+    qs += "&precision=" + config["precision"]
+    headers = {"Authorization": " Token" + config["authorization"], "Content-Type": "text/plain; charset=utf-8"}
+    data = "rate=100,ranFor=5"
+    r = urequests.post(config["url"] + "/api/v" + config["version"] + "/write?" + qs, headers=headers, data=data)
     r.close()
