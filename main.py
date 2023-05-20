f8db9aa7c5482ee564b4d55da6d9b2af3a4e01ea
Elin Angelov <opensuser@gmail.com>
2023-05-20 19:55:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from counter import init as initCounter
 from detector import Detector
 from pump import off as pumpOff, on as pumpOn
 from flows.main import flows
+from metrix import influxdb
 
 wakeFromDeepSleep = reset_cause() == DEEPSLEEP_RESET
 
@@ -14,13 +15,14 @@ async def runnable():
     await conn(config["wifi"])
     pumpOn(config["pump"])
     detector = Detector(initCounter(), config["pump"]["safeTimeout"])
-    metrics = await detector.run()
+    mtrcs = await detector.run()
     pumpOff(config["pump"])
-    flowResult = flows[config["flow"]["type"]](config["flow"], metrics)
+    flowResult = flows[config["flow"]["type"]](config["flow"], mtrcs)
     print(flowResult)
-    print(metrics)
+    print(mtrcs)
     # sleep(20)
     print("sleep for: " + str(flowResult["runAfter"]))
+    influxdb(config["metrics"], mtrcs)
     deepsleep(flowResult["runAfter"] * 1000) # ms
 
 run(runnable())
