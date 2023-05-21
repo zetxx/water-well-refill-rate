d332d2a1ee24683811d9108bc09975719235962a
zetxx <zetxx@belogradchik.biz>
2023-05-21 12:19:21 +0300

    example

---
+++ config.example.py
@@ -0,0 +1,26 @@
+config = {
+    "sensorId": "selqnin1",
+    "wifi": {
+        "ssid": "your ssid",
+        "password": "your ssid's password",
+        "retry": 200
+    },
+    "flow": {
+        "type": "static",
+        "runAfter": 60 * 60 * 3
+    },
+    "pump": {
+        "authorization": "authorization header",
+        "url": "shelly url",# shelly
+        "safeTimeout": 20 # in seconds
+    },
+    "metrics": { # influxdb
+        "org": "?",
+        "bucket": "?",
+        "precision": "s",
+        "version": "2",
+        "authorization": "token",
+        "url": "?",
+        "timestamp": {"url": "timestamp link"}
+    }
+}
